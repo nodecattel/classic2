30c4ff03b9668decda432dada6079cc3396a0a82
blackluv <samuelsimeon550@gmail.com>
2025-02-28 13:16:33 +0100

    changes fix syn

--- src/main.cpp
+++ src/main.cpp
@@ -3528,9 +3528,16 @@ std::vector<unsigned char> GenerateCoinbaseCommitment(CBlock& block, const CBloc
 
 bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& state, const Consensus::Params& consensusParams, CBlockIndex * const pindexPrev, int64_t nAdjustedTime)
 {
+    if (pindexPrev && pindexPrev->nHeight < 112173) {
+        //LogPrintf("Skipping difficulty check for block %d, below block 97000\n", pindexPrev->nHeight);
+        return true;  // Allow block even if proof of work is incorrect for blocks before 97000
+    }
+
     // Check proof of work
-    if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
-        return state.DoS(100, false, REJECT_INVALID, "bad-diffbits", false, "incorrect proof of work");
+    if (pindexPrev->nHeight >= 112173) {
+        if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
+            return state.DoS(100, false, REJECT_INVALID, "bad-diffbits", false, "incorrect proof of work");
+    }
 
     // Check timestamp against prev
     if (block.GetBlockTime() <= pindexPrev->GetMedianTimePast())
