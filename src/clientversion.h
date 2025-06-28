e3a2cf9d389c8ddf686fa68370ccb5b54a7ce5ca
n3k0h1m3 <nekohime@nodecattel.xyz>
2025-06-28 19:07:49 +0700

    fix copyright and updated build doc for linux and windows

--- src/clientversion.h
+++ src/clientversion.h
@@ -38,7 +38,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
+#define COPYRIGHT_STR "2009-2015 The Bitcoin Core developers, " STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Classic developers"
 
 /**
  * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
--- src/util.cpp
+++ src/util.cpp
@@ -805,7 +805,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
     // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+        // Bitcoin Core copyright handled separately in LicenseInfo()
     }
     return strCopyrightHolders;
 }
