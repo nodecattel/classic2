d3b4b69d69cb3aa5a8df835dfcd51158638d9db2
blackluv <samuelsimeon550@gmail.com>
2025-06-30 10:17:03 +0100

    logprintf error on emergency fixed

--- src/pow.cpp
+++ src/pow.cpp
@@ -160,8 +160,8 @@ unsigned int GetNextWorkRequiredNew(const CBlockIndex* pindexLast, const CBlockH
         int64_t time_diff = pblock->GetBlockTime() - pindexLast->GetBlockTime();
         int64_t spacing = params.nPostBlossomPowTargetSpacing;
 
-        LogPrintf("Emergency rule check: height=%d, time_diff=%d, spacing=%d, 3x=%d, 6x=%d, 8x=%d\n",
-                  pindexLast->nHeight, time_diff, spacing, spacing*3, spacing*6, spacing*8);
+        LogPrintf("Emergency rule check: height=%d, time_diff=%lld, spacing=%lld, 3x=%lld, 6x=%lld, 8x=%lld\n",
+          height, time_diff, spacing, spacing * 3, spacing * 6, spacing * 8);
         
         arith_uint256 lastTarget;
         lastTarget.SetCompact(pindexLast->nBits);
